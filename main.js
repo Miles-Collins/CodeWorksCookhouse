53af97d4a8257856241e994768007dad80a5142c
Miles-Collins <miles.collins@ymail.com>
2023-08-09 16:25:31 -0600

    🚀🚀🚀

---
+++ main.js
@@ -0,0 +1,97 @@
+const appetizers = [
+  {
+    name: "HTML5 Hummus Platter",
+    price: 8.99,
+    description:
+      "A platter of assorted hummus flavors served with crispy pita chips and vegetable sticks, a tribute to the foundational HTML5 technology.",
+  },
+  {
+    name: "CSS Crispy Calamari",
+    price: 10.99,
+    description:
+      "Crispy fried calamari rings served with a tangy CSS-inspired dipping sauce, creating a playful contrast of textures."
+  },
+  {
+    name: "Vue Veggies Tempura",
+    price: 9.49,
+    desecription: "Lightly battered and fried seasonal vegetables, served with a Vue.js-inspired sweet chili dipping sauce."
+  },
+]
+
+const mainCourses = [
+  {
+    name: "MVC Mixed Grill",
+    price: 18.99,
+    description: "A combination of grilled meats including C#-seasoned chicken, .Net-marinated steak, and Node-infused lamb chops, served with a side of Bootstrap-baked vegetables." 
+  },
+  {
+    name: "Bootstrap BBQ Burger",
+    price: 13.99,
+    description: "A juicy burger topped with melted cheese, crispy bacon, and Bootstrap-inspired BBQ sauce, served with a side of MVC coleslaw."
+  },
+  {
+    name: "C# Seafood Symphony",
+    price: 22.99,
+    description: "A medley of seared seafood, including CSS-coated scallops, HTML5-crusted salmon, and Vue-infused shrimp, served over a bed of Node-powered risotto."
+  },
+  {
+    name: "Node Noodle Stir-Fry",
+    price: 15.99,
+    description: "Wok-tossed noodles with fresh vegetables and your choice of protein, tossed in a savory Node.js-inspired sauce."
+  },
+  {
+    name:
+    price:
+    description: 
+  }
+]
+
+const sideDishes = [
+  {
+    name: ".NET Nacho Tower",
+    price: 11.99,
+    description: "Layered nachos with melted cheese, black beans, jalapeños, and sour cream, served in a .Net architecture reminiscent presentation."
+  },
+  {
+    name: "Dynamic CSS Salad",
+    price: 7.49,
+    description: "A dynamic salad with mixed greens, roasted nuts, dried fruits, and a customizable array of dressings to suit your tastes."
+  },
+]
+
+const desserts = [
+  {
+    name: "Vue Vanilla Parfait",
+    price: 6.99,
+    description: "A dynamic salad with mixed greens, roasted nuts, dried fruits, and a customizable array of dressings to suit your tastes." 
+  },
+  {
+    name: "Node Nutty Brownie",
+    price: 7.99,
+    description: "Warm chocolate brownie topped with nuts and a scoop of Node.js-powered coffee ice cream, creating a harmonious blend of flavors."
+  },
+  {
+    name: "Responsive Raspberry Cheesecake",
+    price: 8.49,
+    description: "A classic cheesecake infused with fresh raspberries, served with HTML5-inspired raspberry coulis and CSS-spun sugar decorations."
+  },
+]
+
+const beverages = [
+  {
+    name: "CodeBrew Coffee",
+    price: 3.99,
+    description: "A carefully crafted blend of coffee beans from different regions, representing the blend of languages taught at CodeWorks." 
+  },
+  {
+    name: "TechTonic Tea",
+    price: 2.99,
+    description: "A selection of aromatic teas, each named after a programming technology, providing a soothing and refreshing experience."
+  },
+  {
+    name: "Node Nectar",
+    price: 4.49,
+    description: "A fruity and refreshing mocktail infused with Node.js spirit, served with a garnish of fresh herbs and citrus slices." 
+  },
+]
+
