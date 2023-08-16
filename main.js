b6576c850bbd2662bc6317346f54a9a2d1ada099
Miles-Collins <miles.collins@ymail.com>
2023-08-16 10:25:06 -0600

    filter menu

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+// [] .getElementById vs .querySelector
+// [] Element Manipulation classList
+// [] Emphasize single responsibility principle
+// [] Getting a total value from arrays
+// [] Dynamic page rendering
+// [] Working with multiple draw method
+
 const appetizers = [
   {
     name: "HTML5 Hummus Platter",
